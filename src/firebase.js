2bd4f6cc97ee276821d701fd431dceb5f3f8826c
Pulkit <pulkit@Pulkits-MacBook-Air-2.local>
2026-03-14 03:43:01 +0530

    fix: update firebase config with correct credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChGIzwJAHnSWLQRsd4m8_mMO7P3nmAFCI",
+  apiKey: "AIzaSyChGIzwJAHnSWLQRsd4m8_mtwj903IRwmg",
   authDomain: "cloud-cost-control-fc86e.firebaseapp.com",
   projectId: "cloud-cost-control-fc86e",
   storageBucket: "cloud-cost-control-fc86e.firebasestorage.app",
-  messagingSenderId: "823811813370",
-  appId: "1:823811813370:web:4f5ace1de576c3d6e3d7ea"
+  messagingSenderId: "999151125411",
+  appId: "1:999151125411:web:62dc2ff8b4822485a1d016"
 };
 
 const app = initializeApp(firebaseConfig);
